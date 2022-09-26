9a35961069b524045f6217ad84e28e111870d031
gauravshinde1816 <gauravshinde1816@gmail.com>
2022-09-26 08:22:55 +0530

    files changes

--- src/components/preloader.js
+++ src/components/preloader.js
@@ -5,9 +5,8 @@ import Goku1 from "../assets/goku1.png";
 export default function Preloader() {
   const quotes = [
     // {quote: "Programming isn't about what you know; it's about what you can figure out.", author: "Chris Pine"},
-    // {quote: "Life is like riding a bicycle. To keep your balance, you must keep moving.", author: "Albert Einstein"},
+    {quote: "Life is like riding a bicycle. To keep your balance, you must keep moving.", author: "Albert Einstein"},
     {quote: "It's unbelievable how much you don't know about the game you've been playing all your life.", author: "Mickey Mantel"},
-    {quote: "If you know more than others, you will be the loneliest.", author: "Vedant"},
     {quote: "You’ll laugh at your fears when you find out who you really are.", author: "Picollo"}
   ]
   const random = Math.floor(Math.random()*(quotes.length));
